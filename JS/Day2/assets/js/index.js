3560a78a07170303dfa538a75cb48c019c1ee144
JananiS1418 <jananisomasundaram07@gmail.com>
2026-09-22 13:56:44 +0530

    JS Update

---
+++ JS/Day2/assets/js/index.js
@@ -0,0 +1,134 @@
+
+
+
+        // var   userName = 10
+        // let   UserName 
+        // const user_name = 20
+
+
+        // Data Types
+
+        // Primitive
+        // Non primtive
+
+        // primtive 
+        // String - " grter10"
+        // Number - 123467
+        // Boolean - true / false 
+        // undefind - System will return if you declare the varibale
+        // Null - Developer give the intintionally its Empty or null - no value   
+       
+        
+        //  Assinment Operator
+
+        // =
+
+       
+
+// console.log(10);
+// console.log(10+20);   
+
+// var username = 10
+
+// username = 30
+// console.log('username',username);
+
+
+// var username = 20
+
+// let userNames = 20
+
+// userNames = "React"
+
+// console.log('username',username);
+// console.log('userNames',userNames);
+// const userValues = 20
+
+// userValues = 30
+
+// console.log('userValues',userValues);
+
+
+
+
+// 
+// Declaration 
+//  keyword name - undefind
+
+// inti
+
+// keyword name = 10
+// Redeclar
+// key samename
+// key samename
+// Reassin
+// key username
+// username = 30
+
+// Trastional way - var
+
+// let , const - ES6
+
+
+// Var is a Functional Scope
+// let,const - Block Scope
+// Global Scope
+
+// Assinment Operator - 
+// =
+// +=   assign & add
+// -=   Assign & sub
+// *= - assign & muilt
+// /= - assign & divite
+// %= - assign & modulo
+
+// Arithmatic Operator
+
+// +,-,*,/,%,**, 
+
+// comparion operator
+
+// ==
+// ===
+// !==
+// !===
+// >
+// <
+// >=
+// <=
+
+// 20 >= 20
+// 20 > 18
+
+// 20 =< 20
+
+// 6 < 10 
+
+
+// ==
+// ===
+
+// 10 !== 9
+
+// Logical
+
+// &&
+// ||
+// !
+
+// x=10
+// x+10
+// += x=x+68
+// -= x=x-49
+// ++ = x+1
+// -- = x-1
+
+
+// 30 name - one case
+// Variable Return - console print - 5 print
+// init - 10 value - var , let , const 
+
+// String,number,boolean,null,undefind
+
+
+
