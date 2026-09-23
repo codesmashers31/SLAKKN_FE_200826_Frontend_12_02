f2328c4b540008d16b4730c6ce8aa56f1742f134
JananiS1418 <jananisomasundaram07@gmail.com>
2026-09-23 13:58:00 +0530

    updated

---
+++ JS/Day3/assets/js/index.js
@@ -0,0 +1,82 @@
+// title = "Java Script"
+// console.log(title);
+
+// title = "React"
+
+// console.log(title);
+
+
+// let numberData = 1000
+// let numberTwo =  3000
+
+// let total = numberData  + numberTwo
+
+
+// console.log(total);
+// Basic Assinment
+// =
+
+// Arithmatic Op
+
+let firstNumber = 46
+let secondNumber = 20
+let total = 0
+
+let resutlt = firstNumber = secondNumber
+
+console.log(resutlt);
+
+
+secondNumber = total
+
+console.log(secondNumber);
+
+
+
+
+
+
+
+// Mathamaticaly Assinment
+// +=
+
+// x = x+y
+
+// let valueData = 1
+
+// console.log(valueData);
+
+// valueData -= 2
+
+// console.log(typeof valueData);
+// console.log(valueData);
+// valueData -= 3
+
+// console.log(valueData);
+
+
+
+
+
+
+// -=
+// *=
+// /=
+// %=
+// **=
+
+
+
+
+
+
+// 1. String - 18 times - create - 
+
+
+// Reassign  - +=
+
+
+
+
+// 2. Number - 13 Times - Create - Reassign with +=
+
