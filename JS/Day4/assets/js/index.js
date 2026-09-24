c40b9f60c163dd27dd90f377cdd4a3ae35d8e8e6
JananiS1418 <jananisomasundaram07@gmail.com>
2026-09-24 13:38:21 +0530

    Push for loop

---
+++ JS/Day4/assets/js/index.js
@@ -0,0 +1,112 @@
+// commeresan op - 
+// ==
+
+let numberValue = 0
+
+
+
+// console.log(numberValue === numberVlaue1);
+
+// if(numberValue > 18){
+
+//     console.log("This is true");
+    
+// }else if(numberValue===18){
+//     console.log("Your Eligiable to Vote");
+    
+// }else{
+
+//     console.log("Above is False so i showing in console");
+// }
+
+
+// if
+// if(condition){
+
+// }
+// else
+// else if
+// ===
+// !==
+
+// <
+// >
+// >=
+// <=
+
+// if(numberValue!==18){
+
+//     console.log('Running');
+    
+// }
+
+// Logical OP
+
+// !
+
+// 
+// Non 0 Number - True
+// True - True
+// "String" - true
+// " " - true
+// {} - true
+// [] - true
+
+// Falsy value
+
+// 0 false
+// "" - false
+// undefind - false
+// null - false
+// false - 
+// !==
+
+if(!numberValue){
+    console.log('running');
+    
+}
+
+// &&
+// ||
+
+
+
+
+let a = 10
+
+// a+=10
+// a = a+10
+// a--
+// a=a+1
+
+// console.log(a);
+
+
+
+
+
+
+for(let a = 0; a<10; a++){
+   
+  console.log(a);
+  console.log("Running");
+  
+   
+
+}
+
+
+
+
+
+// 2 program 
+
+// 100
+
+// Milk     = 30
+// Bisket   = 10
+// chocolete = 20
+// icecreme = 40
+
+
+// ans
